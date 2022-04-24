b743f7eb2bf6b907ef3f9fa02258cd9f388c18b2
Olga Brazhkina <emelyanova84@yandex.ru>
2022-04-24 14:52:03 +0500

    task_final done Brazhkina Olga

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,36 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    /*
+    let data = {data: 
+        {
+      client: {}, 
+      order: {},
+      //goods: [{title, count}]
+      goods: []
+        }
+    };
+ */
+    let data =  {        
+      client: {}, 
+      order: {},
+      goods: []
+    };   
 
     let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i += 1 ) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
+     }
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    let adr = 'ул. ' + address.street +', дом ' + address.house + ', ' + address.entrance + ' подъезд, '+ address.floor +' этаж, кв ' + address.flat;
+ 
+    data.client = name +' '+ phone;
+    data.order = {address:adr, sum:sum};
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
+    
+    //console.log(adr);
+    //console.log(data);
 
     return jsonData;
 }
